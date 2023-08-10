da50b139153c9eb6048090da26b310d564c9eb03
Jerry Cohen <34323210+jerrycohen@users.noreply.github.com>
2023-08-10 12:50:46 -0700

    Add files via upload

--- bulkprompt-translation.py
+++ bulkprompt-translation.py
@@ -1,3 +1,9 @@
+### Folow me on Linkedin
+### https://linkedin.com/in/jerryscohen
+
+## The below code imports a csv file and sends each row to GPT3.5 for translation into other languages
+## You could modify the prompt template to analyze or transpose the input text in anyway you want.   Send me a DM if you need help.
+
 import openai
 import os
 import json
@@ -15,7 +21,7 @@ def call_openai(prompt_id, prompt):
         response = openai.ChatCompletion.create(   
             model="gpt-3.5-turbo",
             messages=messages,
-            temperature=0.2, # specifies randmonness of model output. higher numbers add more randmoness
+            temperature=0, # specifies randmonness of model output. higher numbers add more randmoness
         )
         return response.choices[0].message["content"]
     except openai.error.OpenAIError as error:
@@ -28,68 +34,83 @@ def remove_trailing_comma(json_string):
     replaced_string = re.sub(pattern, "}", json_string)
     return replaced_string
 
-#this function is used to convert to take the json response and appended it as a row in the output csv file 
-def append_json_to_csv(json_data, csv_file):
-    field_names = [
-        "id",
-        "input_text",
-        "spanish",
-        "french",
-        "russian"
-    ]
 
-    with open(csv_file, 'a', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=field_names)
-
-        # Write headers if the CSV file is empty
-        if file.tell() == 0:
-            writer.writeheader()
-
-        writer.writerow(json_data)
-
-
-def bulk_prompts_from_csv(csv_file):
-    with open(csv_file, 'r') as file:
+def read_csv(file_path, skip_header=True):
+    data = []
+    with open(file_path, 'r') as file:
         reader = csv.reader(file)
-        #next(reader)  # If header rower in import file then skip first row. Comment out if no header row.
-
+        if skip_header:
+            next(reader)  # Skip the header row
         for row in reader:
-            prompt_input_id = row[0]   # assumes a unique ID in the first column
-            prompt_input_text = row[1]  # assumes the input text is in the second column
+            data.append(row)
+    return data
 
-            #below I provided a prompt similar to using ChatGPT. I'm very careful to request that the response is in JSON so my code can reliably consume it 
-            prompt = f"""
-                Your task is to perform the following actions on a User's input text. The input text is copied below in triple backticks. Your only output should be the JSON object described in the last step. Do not output your responses to the subsequent steps.
-
-                1 - Translate the text to Spanish
-
-                2 - Translate the text to French
-
-                3 - Translate the text to Russian
-
-                4 - JSON Output: Create a JSON object that includes the following keys: input_text, spanish, french, russian. The JSON object should represent the final output.
+def write_csv(data, file_path):
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(data)
 
 
-                Input Text:
-                ```{prompt_input_text}```
-            """
+#update the prompt_template to whatever you'd like. this is similar to a prompt you would provide chatgpt. 
+# The JSON "keys" will become the column names in the output CSV
+def process_text_with_gpt(input_id, input_text):
+
+    prompt_template = f"""
+        Your task is to perform the following actions on a user's input text. The input text is copied below and delimted by the characters ####. Your only output should be the JSON object described in the last step. Do not output your responses to the subsequent steps.
+
+        1 - Translate the text to Spanish
+        2 - Translate the text to French
+        3 - Translate the text to Russian
+        4 - JSON Output: Create a JSON object that includes the following keys: spanish, french, russian. The JSON object should represent the final output.
+
+        Input Text:
+        ####{input_text}####
+    """
+
+    gpt_response = call_openai(input_id, prompt_template)
+
+    processed_data = {
+        "id": input_id,
+        "input_text": input_text
+    }
+
+    response_data = json.loads(remove_trailing_comma(gpt_response))
+    processed_data.update(response_data)
+
+    return processed_data
 
 
-            gpt_response = call_openai(prompt_input_id, prompt)
-            #print (gpt_response)
-            
-            json_data = json.loads(remove_trailing_comma(gpt_response))  #converts json string into pythons json object
-            json_data["id"] = prompt_input_id   #adds the input rows id column value to json boject
-            append_json_to_csv(json_data, output_file_path)  #appends to csv output file
-            print (f"ID: {prompt_input_id} added to ouput file")
+def process_text_from_csv(input_file_path, output_file_path):
+    data = read_csv(input_file_path, skip_header=True)
+
+    # Extract the keys names to use them for the csv header 
+    processed_data_keys = None
+    for row in data:
+        prompt_input_id = row[0]
+        prompt_input_text = row[1]
+        processed_data = process_text_with_gpt(prompt_input_id, prompt_input_text)
+        processed_data_keys = list(processed_data.keys())  # Extract keys from the first row
+        break  # We only need keys from the first row
+
+    if not processed_data_keys:
+        print("No data found.")
+        return
+
+    with open(output_file_path, 'w', newline='') as output_file:
+        writer = csv.DictWriter(output_file, fieldnames=processed_data_keys)
+        writer.writeheader()
+
+        for row in data:
+            prompt_input_id = row[0]
+            prompt_input_text = row[1]
+            processed_data = process_text_with_gpt(prompt_input_id, prompt_input_text)
+            writer.writerow(processed_data)
+
+            print(f"ID:{prompt_input_id} added to output file")
+    
+    print("Done")
 
 
-
-input_file_path = './python/bulkprompt/bulkprompt-import.csv'  #replace with whatever location you'd like
-output_file_path = './python/bulkprompt/bulkprompt-export.csv'   #replace with whatever location you'd like
-
-
-bulk_prompts_from_csv(input_file_path)
-
-
-
+input_file_path = './python/bulkprompt/translation-import.csv'
+output_file_path = './python/bulkprompt/translation-export.csv'
+process_text_from_csv(input_file_path, output_file_path)
